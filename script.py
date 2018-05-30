ae805319c6892a3f3b5416ed4e69c385b6ebb5cf
mierzej <mierzejejewski91@gmail.com>
2018-05-30 10:40:46 +0200

    Add new script

---
+++ script.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#
+# Example python script
+#
+
+
+import datetime
+
+date_time = datetime.datetime.now()
+print date_time.strftime('[%Y-%m-%d %H:%M:%S] ') + str('First line of log')
+
